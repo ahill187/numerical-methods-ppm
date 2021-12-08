bb61073fe95fb6a0740a0d95a43418de90f1c520
Ainsleigh Hill <ainsleighhill@gmail.com>
2021-12-08 20:52:49 +0100

    edit linear regression file

--- linear_regression_hydrogen.py
+++ linear_regression_hydrogen.py
@@ -1,24 +1,27 @@
+
 import numpy as np
+import math
 
 
 class Hydrogen:
-    def __init__(self, units="atomic", azimuthal_number=0, energy=1.0):
+    def __init__(self, units="atomic", azimuthal_number=0, energy=-1.0):
         self.units = units
         self.azimuthal_number = azimuthal_number  # quantum number l
         self.energy = energy  # ground state energy in atomic units
 
     def compute_radial_equation_error(self, r_data, R_matrix, T_matrix, coefficients):
-        num_data_points = np.shape(R_matrix)[0]
-        energy_vector = np.full(num_data_points, self.energy)
-        r_inverse_vector = np.array([1/r for r in r_data])
-        r_inverse_squared_vector = np.array([1/r**2 for r in r_data])
-        error = np.matmul(T_matrix, coefficients) - np.matmul(
-            (2*energy_vector + self.azimuthal_number*(self.azimuthal_number + 1)*r_inverse_squared_vector - r_inverse_vector),
-            np.matmul(R_matrix, coefficients))
+        error = np.matmul(T_matrix, coefficients) - self.g_radial_equation(r_data) * np.matmul(R_matrix, coefficients)
         return error
 
+    def g_radial_equation(self, r_data):
+        num_data_points = np.shape(r_data)[0]
+        energy_vector = np.full(num_data_points, self.energy)
+        r_inverse_vector = np.array([1 / r for r in r_data])
+        r_inverse_squared_vector = np.array([1 / r**2 for r in r_data])
+        return (-2 * energy_vector + self.azimuthal_number * (self.azimuthal_number + 1) * r_inverse_squared_vector - r_inverse_vector)
 
-def cost_function(u_pred, r_data, R_matrix, T_matrix, coefficients, hydrogen):
+
+def cost_function(coefficients, u_pred, r_data, R_matrix, T_matrix, hydrogen):
     """Compute cost function.
 
     The radial function for Hydrogen is a second order ODE, which can be set to zero by bringing
@@ -36,7 +39,22 @@ def cost_function(u_pred, r_data, R_matrix, T_matrix, coefficients, hydrogen):
     """
     error = hydrogen.compute_radial_equation_error(r_data, R_matrix, T_matrix, coefficients)
     squared_difference = np.dot(error, error)
-    return squared_difference/len(r_data)
+    return squared_difference / len(r_data)
+
+
+def cost_function_deriv(coefficients, u_pred, r_data, R_matrix, T_matrix, hydrogen):
+    error = hydrogen.compute_radial_equation_error(r_data, R_matrix, T_matrix, coefficients)
+    g_vector = hydrogen.g_radial_equation(r_data)
+    polynomial_degree = np.shape(R_matrix)[1]
+    num_data_points = np.shape(R_matrix)[0]
+    J_deriv = []
+    for k in range(0, polynomial_degree):
+        identity = np.zeros(polynomial_degree)
+        identity[k] = 1.0
+        J_deriv_k = 2 / num_data_points * np.dot(error, np.matmul(T_matrix, identity) - \
+                    np.matmul(T_matrix, identity) * g_vector)
+        J_deriv.append(J_deriv_k)
+    return np.array(J_deriv)
 
 
 def create_R_matrix(num_data_points, polynomial_degree, r_data):
@@ -88,9 +106,21 @@ def create_T_matrix(num_data_points, polynomial_degree, r_data):
     row = 0
     for r in r_data:
         for n in range(2, polynomial_degree):
-            T_matrix[row][n] = n*(n-1)*r**(n-2)
+            T_matrix[row][n] = n * (n - 1) * r**(n - 2)
         row = row + 1
     return T_matrix
+
+
+def gradient_descent(coefficients, alpha, max_cost, max_iterations, cost_function, cost_function_deriv, *args):
+
+    for n in range(0, max_iterations):
+        J_deriv = cost_function_deriv(coefficients, *args)
+        cost = cost_function(coefficients, *args)
+        # print(alpha * J_deriv)
+        coefficients = coefficients - alpha * J_deriv
+        if cost <= max_cost:
+            break
+    return coefficients, cost
 
 
 def main():
@@ -102,9 +132,16 @@ def main():
     R_matrix = create_R_matrix(num_data_points, polynomial_degree, r_data)
     T_matrix = create_T_matrix(num_data_points, polynomial_degree, r_data)
     coefficients = np.ones(polynomial_degree)
+    for n in range(0, polynomial_degree):
+        coefficients[n] = (-1)**n * (1 / math.factorial(n)) * 1 / np.sqrt(np.pi)
     u_pred = np.matmul(R_matrix, coefficients)
-    cost = cost_function(u_pred, r_data, R_matrix, T_matrix, coefficients, hydrogen)
+    coefficients, cost = gradient_descent(coefficients, alpha, max_cost, max_iterations, cost_function,
+                                    cost_function_deriv, u_pred, r_data, R_matrix, T_matrix,
+                                    coefficients, hydrogen)
 
 
-if __name__ == "__main__":
-    main()
+
+
+#
+# if __name__ == "__main__":
+#     main()
